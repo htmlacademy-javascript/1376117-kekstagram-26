6711e7c616a077eddcc9eb5f469d437ad2c97c85
AnnaPikaleva <97855987+AnnaPikaleva@users.noreply.github.com>
2022-06-15 00:41:14 +0300

    Добавляет больше деталей

--- js/main.js
+++ js/main.js
@@ -35,3 +35,105 @@ getRandomIntInclusive(1,5);
 const checkStringLength = (stringToCheck, maxLength) => stringToCheck.length <= maxLength;
 
 checkStringLength('Keks', 10);
+
+const descriptions = [
+  'Отель около моря',
+  'Отличный указатель',
+  'Море',
+  'Девушка с фотоаппаратом',
+  'Блюдо: "Спа процедуры"',
+  'Черный автомобиль',
+  'Клубничка',
+  'Витаминки!',
+  'Улыбаемся и машем',
+  'Обувница',
+  'Пляж',
+  'Ауди',
+  'Здоровое питание!',
+  'Суши кот!',
+  'Комфорт - превыше всего',
+  'Завораживает!',
+  'Отличная музыка!',
+  'Классика',
+  'Технологичные домашние тапочки!',
+  'Пальмы',
+  'Вкуснее ничего не ел(а)',
+  'Закат',
+  'Крабик',
+  'Концерт',
+  'Внедорожник',
+];
+
+const authorNames = [
+  'Павел',
+  'Вова',
+  'Катя',
+  'Маша',
+  'Сергей',
+  'Анна',
+  'Вероника',
+  'Роман',
+  'Стас',
+  'Антон',
+  'Лиза',
+  'Яна',
+  'Кристина',
+  'Валентин',
+  'Валентина',
+  'Женя',
+  'Саша',
+  'Петя',
+  'Алексей',
+  'Оля',
+  'Андрей',
+  'Михаил',
+  'Алена',
+  'Олеся',
+  'Даша',
+];
+
+const commentBank = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+const getRandomArrayElements = (elements) => elements[getRandomIntInclusive(0, elements.length - 1)];
+
+const createPhotoInfo = (id, description, comments) =>
+  {
+    id,
+    url: 'photos/' + id + '.jpg',
+    description,
+    likes: getRandomIntInclusive(15,200),
+    comments,
+  };
+
+const makeComment = (id) => {
+  return {
+    id,
+    avatar: 'img/avatar-' + getRandomIntInclusive(1,6) + '.svg',
+    message: getRandomArrayElements(commentBank),
+    name: getRandomArrayElements(authorNames),
+  };
+};
+
+const makePhotos = (count) => {
+  const photos = [];
+  for (let i = 0; i <= count - 1; i++) {
+    const messages = [];
+    messages.push(makeComment(i + 1), makeComment(i + 2));
+    photos.push(createPhotoInfo((i + 1), descriptions[i], messages));
+  };
+  return photos;
+};
+const maxPhotoCount = 25;
+makePhotos(maxPhotoCount);
+
+const similarPhotoInfo = Array.from({length: 25}, createPhotoInfo);
+
+console.log(similarPhotoInfo);
+
