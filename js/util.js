01b5b32686cd9a8571814208d0a3d8d300cfccc0
AnnaPikaleva <97855987+AnnaPikaleva@users.noreply.github.com>
2022-06-18 10:25:47 +0300

    Разбивает на модули

---
+++ js/util.js
@@ -0,0 +1,37 @@
+// Функция, возвращающая случайное целое число.
+// Источник https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+
+const getRandomIntInclusive = (min, max) => {
+  if (min >= 0 && max >= 0) {
+    if( min < max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    else if (min > max) {
+      const minValue = max;
+      max = min;
+      min = minValue;
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    else {
+      return min;
+    }
+  }
+  else {
+    return 'Number must be positive';
+  }
+};
+
+// Функция для проверки максимальной длины строки
+// Источник https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/length
+
+const checkStringLength = (stringToCheck, maxLength) => stringToCheck.length <= maxLength;
+
+checkStringLength('Keks', 10);
+
+export {getRandomIntInclusive};
